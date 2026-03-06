5f414e1c04054c9e2ea627c864f3bc6a88e85c7a
Evelyn Ruiz <L22260064@matamoros.tecnm.mx>
2026-03-06 12:14:01 -0600

    Usuarios(modificaciones breves)

--- routes/web.php
+++ routes/web.php
@@ -10,27 +10,21 @@ use App\Http\Controllers\UsuariosController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 // Rutas de autenticación
 Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-
 // Rutas protegidas
 Route::middleware('auth')->group(function (){
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
-
 Route::middleware(['auth', 'user_type:admin'])->group(function () {
     
     Route::resource('categorias', CategoriasController::class);
     Route::resource('libros', LibroController::class);
 
     Route::get('/usuarios', [UsuariosController::class, 'index'])->name('usuarios.index');
-
 });
-
 Route::middleware(['auth', 'user_type:user'])->group(function () {
-
 });
