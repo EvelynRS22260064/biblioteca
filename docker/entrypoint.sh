aa5dc1116fc215ed3a9df55eb4bf6eb93cb4c047
EvelynRS22260064 <L22260064@matamoros.tecnm.mx>
2026-04-14 11:36:10 -0500

    Creación del Docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Iniciar PHP-FPM en segundo plano
+php-fpm &
+
+# Esperar un poco
+sleep 3
+
+# Generar key si no existe
+php artisan key:generate --force
+
+# Migraciones (opcional)
+php artisan migrate --force
+
+# Cachear config
+php artisan config:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Iniciar Nginx
+nginx -g "daemon off;"
